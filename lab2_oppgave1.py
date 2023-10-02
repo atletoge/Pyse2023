2fc2e50b54e8e6b43296b8ce8fb4904e9f114439
Atle Tøge <atle@dhcp-10-22-78-239.wlan.ntnu.no>
2023-10-02 15:28:10 +0200

    smile

--- lab2_oppgave1.py
+++ lab2_oppgave1.py
@@ -10,7 +10,7 @@ n = 5
 m = 5
 Q_min = 0.5
 price = 0
-
+q = 1
 
 def user_time():
     return np.random.exponential(1)
@@ -27,7 +27,7 @@ def user3_generator(env):
 
 def calculate_Q(m,n,k):
     #m - antall resource slots, n - antall servere, k - antall brukere
-    return min(1, (m*n)/k)
+    q = min(1, (m*n)/k)
 
 def calculate_MOS(Q):
     if Q >= 0.9:
@@ -45,24 +45,35 @@ def calculate_MOS(Q):
 
 def user3(env, id):
     k = k+1
-    if Q_min < calculate_Q(m,n,k):
+    add_server()
+    calculate_Q(m,n,k)
+    if Q_min < q:
         print(f'User {id} logged in')
         user_login = env.now
+        userPrice = calculate_price_for_user()
         yield env.timeout(user_time())
         time_active = env.now - user_login
-        print(f'User {id} was active for {time_active} minutes')
+        calculate_Q(m,n,k)
+        print(f'User {id} was active for {time_active} minutes and had a bandwidth of {q} for the price of {userPrice} NOK.')
         k = k-1
+        calculate_Q(m,n,k)
     else:
         k = k-1
+        calculate_Q(m,n,k)
+    remove_server()
 
 
 def add_server():
-    if (n<11):
+    if (n<10):
         n = n+1
 
 def remove_server():
     if (n>2):
         n = n-1
+
+def calculate_price_for_user():
+    totalPower = (m*n)*200
+    return (totalPower/k)*price
 
 def check_price_level():
     p_low = 0.1
@@ -70,16 +81,20 @@ def check_price_level():
     p_high = 5
     Liste = ["low", "high"]
     while True:
+        add_server()
         price = p_low
         yield env.timeout(1)
+        remove_server()
         while True:
             price = p_medium
             yield env.timeout(1)
             if(random.choice(Liste) == "low"): #Bruker bare random.choice her siden sannsynlighetsvariabelen p ikke er definert i oppgavetekstene, blir i praksis 0.5
                 break
             else:
+                remove_server()
                 price = p_high
                 yield env.timeout(2)
+                add_server()
        
         
 
